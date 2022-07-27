98a2722c557a979b4ece8186b96db8989be082cf
fanfanboom <zhangyunfan716@gmail.com>
2022-07-27 10:46:01 +0800

    用户中心建设完毕

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,192 +1,134 @@
 <template>
-    <div>
-        <el-row :gutter="20">
-            <el-col :span="12">
-                <el-card shadow="hover">
-                    <template #header>
-                        <div class="clearfix">
-                            <span>基础信息</span>
-                        </div>
-                    </template>
-                    <div class="info">
-                        <div class="info-image" @click="showDialog">
-                            <img :src="avatarImg" />
-                            <span class="info-edit">
-                                <i class="el-icon-lx-camerafill"></i>
-                            </span>
-                        </div>
-                        <div class="info-name">{{ name }}</div>
-                        <div class="info-desc">不可能！我的代码怎么可能会有bug！</div>
-                    </div>
-                </el-card>
-            </el-col>
-            <el-col :span="12">
-                <el-card shadow="hover">
-                    <template #header>
-                        <div class="clearfix">
-                            <span>账户编辑</span>
-                        </div>
-                    </template>
-                    <el-form label-width="90px">
-                        <el-form-item label="用户名："> {{ name }} </el-form-item>
-                        <el-form-item label="旧密码：">
-                            <el-input type="password" v-model="form.old"></el-input>
-                        </el-form-item>
-                        <el-form-item label="新密码：">
-                            <el-input type="password" v-model="form.new"></el-input>
-                        </el-form-item>
-                        <el-form-item label="个人简介：">
-                            <el-input v-model="form.desc"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-button type="primary" @click="onSubmit">保存</el-button>
-                        </el-form-item>
-                    </el-form>
-                </el-card>
-            </el-col>
-        </el-row>
-        <el-dialog title="裁剪图片" v-model="dialogVisible" width="600px">
-            <vue-cropper ref="cropper" :src="imgSrc" :ready="cropImage" :zoom="cropImage" :cropmove="cropImage"
-                style="width: 100%; height: 400px"></vue-cropper>
-
-            <template #footer>
-                <span class="dialog-footer">
-                    <el-button class="crop-demo-btn" type="primary">选择图片
-                        <input class="crop-input" type="file" name="image" accept="image/*" @change="setImage" />
-                    </el-button>
-                    <el-button type="primary" @click="saveAvatar">上传并保存</el-button>
-                </span>
-            </template>
-        </el-dialog>
-    </div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <template #header>
+            <div class="clearfix">
+              <span>基础信息</span>
+            </div>
+          </template>
+          <div class="info">
+            <div class="info-image">
+              <img :src="avatarImg"/>
+            </div>
+            <div class="info-name">{{ name }}</div>
+            <div class="info-desc">好好学习，天天向上！</div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card shadow="hover">
+          <template #header>
+            <div class="clearfix">
+              <span>账户编辑</span>
+            </div>
+          </template>
+          <el-form label-width="90px" :model="form" :rules="formRules" ref="formRef">
+            <el-form-item label="用户："> {{ name }}</el-form-item>
+            <el-form-item label="旧密码：" prop="old">
+              <el-input type="password" v-model="form.old" show-password></el-input>
+            </el-form-item>
+            <el-form-item label="新密码：" prop="new">
+              <el-input type="password" v-model="form.new" show-password></el-input>
+            </el-form-item>
+            <el-form-item>
+              <i class="info">（Tips：新密码至少为8个字符，至少包含1个大写字母，1个小写字母和1个数字）</i>
+            </el-form-item>
+            <el-form-item label="个人简介：">
+              <el-input v-model="form.desc" disabled></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="onSubmit">保存</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
-import VueCropper from "vue-cropperjs";
-import "cropperjs/dist/cropper.css";
+import {reactive, ref} from "vue";
 import avatar from "../assets/img/aha.jpg";
+import {ElMessage} from "element-plus";
+import service from "../utils/request";
+
 export default {
-    name: "user",
-    components: {
-        VueCropper,
-    },
-    setup() {
-        const name = localStorage.getItem("ms_username");
-        const form = reactive({
-            old: "",
-            new: "",
-            desc: "不可能！我的代码怎么可能会有bug！",
-        });
-        const onSubmit = () => {};
+  name: "user",
+  setup() {
+    const name = JSON.parse(sessionStorage.getItem("loginUser")).personName;
+    const form = reactive({
+      old: "",
+      new: "",
+      desc: "好好学习，天天向上！",
+    });
+    const formRules = {
+      old: [
+        {required: true, message: "请输入密码", trigger: "blur"},
+      ],
+      new: [
+        {required: true, message: "请输入密码", trigger: "blur"},
+        {pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[^]{8,16}', message: '新密码不符合规则要求'}
+      ],
+    };
+    const formRef=ref(null);
+    const onSubmit = () => {
+      formRef.value.validate(valid=>{
+        if (valid) {
+          if(form.old === JSON.parse(sessionStorage.getItem("loginUser")).password){
+            service.post(`/api/user/repass/${form.new}`).then(res=>{
+              sessionStorage.setItem("loginUser", JSON.stringify(res.obj));
+              form.old="";
+              form.new="";
+            })
+          }else{
+            ElMessage.error("旧密码错误，不允许修改密码");
+          }
+        }
+      })
+    };
 
-        const avatarImg = ref(avatar);
-        const imgSrc = ref("");
-        const cropImg = ref("");
-        const dialogVisible = ref(false);
-        const cropper = ref(null);
+    const avatarImg = ref(avatar);
 
-        const showDialog = () => {
-            dialogVisible.value = true;
-            imgSrc.value = avatarImg.value;
-        };
-
-        const setImage = (e) => {
-            const file = e.target.files[0];
-            if (!file.type.includes("image/")) {
-                return;
-            }
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                dialogVisible.value = true;
-                imgSrc.value = event.target.result;
-                cropper.value && cropper.value.replace(event.target.result);
-            };
-            reader.readAsDataURL(file);
-        };
-
-        const cropImage = () => {
-            cropImg.value = cropper.value.getCroppedCanvas().toDataURL();
-        };
-
-        const saveAvatar = () => {
-            avatarImg.value = cropImg.value;
-            dialogVisible.value = false;
-        };
-
-        return {
-            name,
-            form,
-            onSubmit,
-            cropper,
-            avatarImg,
-            imgSrc,
-            cropImg,
-            showDialog,
-            dialogVisible,
-            setImage,
-            cropImage,
-            saveAvatar,
-        };
-    },
+    return {
+      name,
+      form,
+      avatarImg,
+      formRules,
+      formRef,
+      onSubmit,
+    };
+  },
 };
 </script>
 
 <style scoped>
 .info {
-    text-align: center;
-    padding: 35px 0;
+  text-align: center;
+  padding: 35px 0;
 }
+
 .info-image {
-    position: relative;
-    margin: auto;
-    width: 100px;
-    height: 100px;
-    background: #f8f8f8;
-    border: 1px solid #eee;
-    border-radius: 50px;
-    overflow: hidden;
+  position: relative;
+  margin: auto;
+  width: 100px;
+  height: 100px;
+  background: #f8f8f8;
+  border: 1px solid #eee;
+  border-radius: 50px;
+  overflow: hidden;
 }
+
 .info-image img {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
-.info-edit {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    opacity: 0;
-    transition: opacity 0.3s ease;
-}
-.info-edit i {
-    color: #eee;
-    font-size: 25px;
-}
-.info-image:hover .info-edit {
-    opacity: 1;
-}
+
 .info-name {
-    margin: 15px 0 10px;
-    font-size: 24px;
-    font-weight: 500;
-    color: #262626;
-}
-.crop-demo-btn {
-    position: relative;
-}
-.crop-input {
-    position: absolute;
-    width: 100px;
-    height: 40px;
-    left: 0;
-    top: 0;
-    opacity: 0;
-    cursor: pointer;
+  margin: 15px 0 10px;
+  font-size: 24px;
+  font-weight: 500;
+  color: #262626;
 }
 </style>
