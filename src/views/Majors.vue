7907bb3ba17016b38c07188924964de1be9cea06
fanfanboom <zhangyunfan716@gmail.com>
2022-07-28 15:48:48 +0800

    新增新增专业功能

--- src/views/Majors.vue
+++ src/views/Majors.vue
@@ -9,10 +9,11 @@
     </div>
     <div class="container">
       <div style="margin-bottom: 10px">
-        <el-select v-model="belongDepartment" placeholder="归属学院" style="margin-right: 10px">
+        <el-select v-model="departmentId" placeholder="归属学院" style="margin-right: 10px">
           <el-option v-for="(item,index) in departments" :label="item.name" :value="item.id" :key="index"/>
         </el-select>
         <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+        <el-button type="warning" icon="el-icon-plus" @click="dialogVisible=true">新增专业</el-button>
       </div>
       <el-table :data="pagedData.content" border>
         <el-table-column label="序号" width="50" align="center">
@@ -30,6 +31,29 @@
                        @current-change="handlePageChange"></el-pagination>
       </div>
     </div>
+
+    <el-dialog title="新增专业" v-model="dialogVisible" width="40%">
+      <el-form label-width="70px" :model="newMajor">
+        <el-form-item label="新专业id">
+          <el-input v-model="newMajor.id"></el-input>
+        </el-form-item>
+        <el-form-item label="专业名称">
+          <el-input v-model="newMajor.name"></el-input>
+        </el-form-item>
+        <el-form-item label="归属学院">
+          <el-select v-model="newMajor.department.id" placeholder="归属学院">
+            <el-option v-for="(item,index) in departments" :label="item.name" :value="item.id" :key="index"/>
+          </el-select>
+        </el-form-item>
+        <el-tag type="danger" size="large">Tips:专业属于系统重要基础数据，请联系学校确定信息无误后准确录入。</el-tag>
+      </el-form>
+      <template #footer>
+                <span>
+                    <el-button @click="dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="handleAdd">确 定</el-button>
+                </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -51,11 +75,19 @@ export default {
         }
       },
       departments: [],
-      belongDepartment: ''
+      departmentId: '',
+      dialogVisible:false,
+      newMajor:{
+        id:'',
+        name:'',
+        department:{
+          id:''
+        }
+      }
     });
     const getData = () => {
       service.get(`/api/major/query/${state.pagedData.pageable.pageNumber}/${state.pagedData.pageable.pageSize}
-      /ASC/department?departmentId=${state.belongDepartment}`).then(res => {
+      /ASC/department?departmentId=${state.departmentId}`).then(res => {
         state.pagedData = res.obj;
       })
     }
@@ -74,9 +106,17 @@ export default {
       state.pagedData.pageable.pageNumber=1;
       getData();
     }
+    const handleAdd = () => {
+      service.post("/api/major/add",state.newMajor).then(()=>{
+        state.dialogVisible=false;
+        //设置回到第一页，以免页数错误导致查询失败
+        state.pagedData.pageable.pageNumber=1;
+        getData();
+      })
+    }
     return {
       ...toRefs(state),
-      handlePageChange, handleSearch
+      handlePageChange, handleSearch,handleAdd
     }
   },
 }
